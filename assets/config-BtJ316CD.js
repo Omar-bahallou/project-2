544eee5600dbaae4260cdffd60f2402e2f75a580
Omar-bahallou <omar.bahallou.05@gmail.com>
2025-01-30 02:14:47 +0100

    Updates

---
+++ assets/config-BtJ316CD.js
@@ -0,0 +1,5 @@
+const o="Omar-bahallou",s="cyber security",a=["Developer","Designer","Security Systems ","Networking"],n="https://bahallou.com",e="Omar-bahallou",t="https://github.com/Omar-bahallou",c="omar_bh3378",i="people/%E1%8F%B0%E1%8E%BB-%E1%8F%AB%E1%8E%B7%E1%8E%AF%E1%8E%A1/pfbid0JVdHfKF5UABRQvsuGCGfzvYxZbYEGqw4HT3EP2x6ShEniWKZcCEdFpGLvzZvX3xol/?rdid=TC855CBNyCzrAHgm&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15h5d5uNtz%2F",r="omar-bahallou-23026b337/",l=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,b={name:o,role:s,disciplines:a,url:n,github:e,repo:t,instagram:c,facebook:i,linkedin:r,ascii:l};export{b as c};
---
+++ assets/spr-lesson-builder-dark-D3wjSSYG.js
@@ -0,0 +1,1 @@
+const s="/assets/spr-lesson-builder-dark-fiDr79ek.jpg",r="/assets/spr-lesson-builder-dark-placeholder-BYjrS8rr.jpg",e="/assets/spr-lesson-builder-dark-fiDr79ek.jpg";export{s as a,r as b,e as s};
